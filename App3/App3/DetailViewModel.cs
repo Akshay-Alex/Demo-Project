0fbf8846975eae474bbea8cfd60f8bb1de52a2dc
Mathew Alex <mathewa@qburst.com>
2021-07-14 15:09:56 +0530

    Add project files.

---
+++ App3/App3/DetailViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public class DetailViewModel : TriggerAction<ImageButton>,INotifyPropertyChanged
+    {
+        private bool nameValid = false;
+        public string testdata = "testdata";
+        private bool genderValid = false;
+        bool _hidePassword = true;
+        public string ShowIcon { get; set; }
+        public string HideIcon { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected override void Invoke(ImageButton sender)
+        {
+            sender.Source = HidePassword ? ShowIcon : HideIcon;
+            HidePassword = !HidePassword;
+        }
+        public bool HidePassword
+        {
+            set
+            {
+                if (_hidePassword != value)
+                {
+                    _hidePassword = value;
+
+                    OnPropertyChanged();
+                }
+            }
+            get => _hidePassword;
+        }
+
+
+        public bool GenderValid
+        {
+            get => genderValid;
+            set
+            {
+                genderValid = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool NameValid
+        {
+            get => nameValid;
+            set
+            {
+                nameValid = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Testdata
+        {
+            get { return testdata; }
+            set { testdata = value; }
+        }
+
+    }
+}
---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public DetailViewModel myviewmodel = new DetailViewModel();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = myviewmodel;
+            myviewmodel.GenderValid = false;
+            myviewmodel.NameValid = false;
+            List<String> myList = new List<String>();
+            myList.Add("Male");
+            myList.Add("Female");
+            List<String> mycurrency = new List<String>();
+            mycurrency.Add("USD");
+            mycurrency.Add("TSH");
+            comboBox.ComboBoxSource = myList;
+            currency.ComboBoxSource = mycurrency;
+            currency.SelectedIndex = 1;
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //string namevalue = namebox.Text;
+            //Navigation.PushModalAsync(new NavigationPage(new Welcomepage(namebox.Text)));
+        }
+
+        private void comboBox_SelectionChanged(object sender, Syncfusion.XForms.ComboBox.SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedIndex > -1)
+            {
+                myviewmodel.GenderValid = true;
+            }
+            else
+            {
+                myviewmodel.GenderValid = false;
+            }
+        }
+
+        private void currency_SelectionChanged(object sender, Syncfusion.XForms.ComboBox.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
